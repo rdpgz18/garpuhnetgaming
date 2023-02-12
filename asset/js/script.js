150a59174c9d77f8f545ea6ff46d68fcdd241ac4
ridwan <119526813+rdpgz18@users.noreply.github.com>
2023-02-12 23:02:53 +0700

    Add files via upload

---
+++ asset/js/script.js
@@ -0,0 +1,27 @@
+document.getElementsByClassName("toggler-navbar")[0].addEventListener("click", toogleClass);
+
+function toogleClass() {
+    document.getElementsByClassName("hamburger-menu")[0].classList.toggle('open')
+    document.getElementsByClassName("sidebar")[0].classList.toggle('open')
+}
+
+//slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
